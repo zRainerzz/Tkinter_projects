981799e41cc0140feddb4f09df6106d75fea1aa1
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-26 15:37:18 +0100

    0

--- tkinter_3_CreatingButtons.py
+++ tkinter_3_CreatingButtons.py
@@ -1,27 +1,36 @@
 from tkinter import *
 
+
+
 root=Tk()
 
-e=Entry(root,width=50,bg="black",fg="white",borderwidth=5)
-e.grid(row=3,column=6)
+
 
 def MyClick():
-    mylabel1=Label(root,text="Look I Clicked A Button!").grid(row=5,column=0)
+    mylabel1=Label(root,text=e.get()).grid(row=5,column=0)
+
+
 
 mybutton1= Button(root,text="Click Me!",padx=50, command=MyClick, fg="blue", bg="white")
 #command allows the execute of a function.
 #padx change the x abscissa axis
 #pady change the y coordinate axis
 #by adding disabled to the button, now it is disabled
+
+
+
 mybutton2=Button(root,text="You can't click me.",pady=50,state=DISABLED,bg="black",fg="white")
 #fg is for colors, bg is for background color.
-
-
+e=Entry(root,width=50,bg="black",fg="white",borderwidth=5)
+e.grid(row=3,column=6)
+e.get()
+#get function gets whatever u typed in the entry widget
 
 
 
 mybutton1.grid(row=0,column=0)
 mybutton2.grid(row=0,column=1)
+
 
 
 root.mainloop()
