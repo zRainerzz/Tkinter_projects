f69c5b7845b0dfe5ab2debfee59fd31c342e845f
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-26 12:32:23 +0100

    ..

--- tkinter_1.py
+++ tkinter_1.py
@@ -6,6 +6,8 @@ root= Tk()
 mostly called root, the root is the main widget (the graphical big box), so we generally start with it.
 """
 
+my_label=Label(root,"Hello World.")
+#so the we added 2 values, firstly is where to appear and secondly the message that will appear.
 
 """
 to create anything in tkinter is 2 steps process:
