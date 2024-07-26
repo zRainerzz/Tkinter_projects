578cf4bebad51ed872e20cdf5e66d0678b81f5f9
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-26 12:30:35 +0100

    .

--- tkinter_1.py
+++ tkinter_1.py
@@ -5,3 +5,10 @@ root= Tk()
 """
 mostly called root, the root is the main widget (the graphical big box), so we generally start with it.
 """
+
+
+"""
+to create anything in tkinter is 2 steps process:
+            1- You have to define the thing you created.
+            2- You have to put it up on the screen.
+"""
