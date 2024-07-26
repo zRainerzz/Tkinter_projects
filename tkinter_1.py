ccdfac72c6975107aa141bbcb8aa636ece9115d9
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-26 14:00:42 +0100

    .

--- tkinter_1.py
+++ tkinter_1.py
@@ -1,17 +1,12 @@
 from tkinter import *
-
-root= Tk()
-
 """
 mostly called root, the root is the main widget (the graphical big box), so we generally start with it.
 """
 
-my_label=Label(root,"Hello World.")
-#so the we added 2 values, firstly is where to go (the root for sure) and secondly the message that will appear.
+root= Tk()
 
+#creating a label widget.
+my_label=Label(root,"Hello World.")
+
+#shoving it onto the screen. 
 my_label.pack()
-"""
-to create anything in tkinter is 2 steps process:
-            1- You have to define the thing you created.
-            2- You have to put it up on the screen.
-"""
