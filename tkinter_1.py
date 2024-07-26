88af69911f5e99e3bbc86d325e45dbde2f66801d
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-26 14:13:05 +0100

    ..

--- tkinter_1.py
+++ tkinter_1.py
@@ -6,7 +6,10 @@ mostly called root, the root is the main widget (the graphical big box), so we g
 root= Tk()
 
 #creating a label widget.
-my_label=Label(root,"Hello World.")
+my_label=Label(root,text="Hello World.")
 
 #shoving it onto the screen. 
 my_label.pack()
+
+#that way it is constantly looping, clarify the program keeps working (constant loop until you kill the whole proccess by ticking the red button.)
+root.mainloop()
