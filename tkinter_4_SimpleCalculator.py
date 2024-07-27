f6c901904d7795faeb8bc21021500907b9942683
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-27 12:18:02 +0100

    ..

--- tkinter_4_SimpleCalculator.py
+++ tkinter_4_SimpleCalculator.py
@@ -7,7 +7,10 @@ e=Entry(root,width=35,borderwidth=5)
 e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
 #e.insert(0, "")
 
-button_1=
+def button_add():
+    
+
+button_1= Button(root,text="1",padx=40,pady=20,command=button_add)
 
 myButton=Button(root,text="Enter Your Stock Quote",command=myClick)
 
