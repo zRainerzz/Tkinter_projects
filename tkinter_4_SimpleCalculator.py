7d115cdcb0a4316ea4cc261a027e78392dc6425f
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-27 12:16:30 +0100

    0

--- tkinter_4_SimpleCalculator.py
+++ tkinter_4_SimpleCalculator.py
@@ -5,7 +5,7 @@ root.title("Calculator")
 
 e=Entry(root,width=35,borderwidth=5)
 e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
-e.insert(0, "Enter Your Name: ")
+e.insert(0, "")
 
 def myClick():
     hello="Hello" + e.get()
