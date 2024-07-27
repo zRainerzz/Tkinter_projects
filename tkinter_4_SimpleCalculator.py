f642ae325c61f826539ba31193c6c7a4d96ff50d
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-27 14:33:10 +0100

    .

--- tkinter_4_SimpleCalculator.py
+++ tkinter_4_SimpleCalculator.py
@@ -16,6 +16,12 @@ def button_click(number):
 def button_clear1():
     e.delete(0,END)
 
+def button_add1():
+    first_number=e.get()
+    global f_int_number
+    f_int_number=int(first_number)
+    e.delete(0,END)
+
 
 #define buttons
 button_1= Button(root,text="1",padx=40,pady=20,command=lambda: button_click(1))
@@ -28,9 +34,9 @@ button_7= Button(root,text="7",padx=40,pady=20,command=lambda: button_click(7))
 button_8= Button(root,text="8",padx=40,pady=20,command=lambda: button_click(8))
 button_9= Button(root,text="9",padx=40,pady=20,command=lambda: button_click(9))
 button_0= Button(root,text="0",padx=40,pady=20,command=lambda: button_click(0))
-button_add= Button(root,text="+",padx=39,pady=20,command=lambda: button_click())
+button_add= Button(root,text="+",padx=39,pady=20,command=button_add1())
 button_equal= Button(root,text="=",padx=91,pady=20,command=lambda: button_click())
-button_clear=Button(root,text="Clear",padx=79,pady=20,command=lambda: button_clear())
+button_clear=Button(root,text="Clear",padx=79,pady=20,command=button_clear1)
 
 
 
