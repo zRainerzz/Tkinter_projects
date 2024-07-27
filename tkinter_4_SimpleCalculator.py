39cbdb6c22f3292d1bba55fb0cfd9b07d54a1747
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-27 14:50:08 +0100

    ..

--- tkinter_4_SimpleCalculator.py
+++ tkinter_4_SimpleCalculator.py
@@ -38,9 +38,15 @@ button_7= Button(root,text="7",padx=40,pady=20,command=lambda: button_click(7))
 button_8= Button(root,text="8",padx=40,pady=20,command=lambda: button_click(8))
 button_9= Button(root,text="9",padx=40,pady=20,command=lambda: button_click(9))
 button_0= Button(root,text="0",padx=40,pady=20,command=lambda: button_click(0))
-button_add= Button(root,text="+",padx=39,pady=20,command=button_add1)
+
 button_equal= Button(root,text="=",padx=91,pady=20,command=button_equal1)
 button_clear=Button(root,text="Clear",padx=79,pady=20,command=button_clear1)
+
+button_add= Button(root,text="ADD(+)",padx=39,pady=20,command=button_add1)
+button_subtract= Button(root,text="SUBTRACT(-)",padx=39,pady=20,command=button_subtract1)
+button_multiply= Button(root,text="MULTIPLY(*)",padx=39,pady=20,command=button_multiply1)
+button_divide= Button(root,text="MULTIPLY(/)",padx=39,pady=20,command=button_divide1)
+
 
 #put buttons on the screen
 button_1.grid(row=3,column=0)
@@ -59,5 +65,9 @@ button_0.grid(row=4,column=0)
 button_clear.grid(row=4,column=1,columnspan=2)
 button_add.grid(row=5,column=0)
 button_equal.grid(row=5,column=1,columnspan=2)
+
+button_subtract.grid(row=6,column=0)
+button_multiply.grid(row=6,column=1)
+button_divide.grid(row=6,column=2)
 
 root.mainloop()
