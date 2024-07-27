fd7efae155f5583b46b5ba2fd76e907fcffb7b34
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-27 12:09:43 +0100

    .

--- tkinter_4_SimpleCalculator.py
+++ tkinter_4_SimpleCalculator.py
@@ -1,6 +1,19 @@
-from tkinter import*
+from tkinter import *
 root=Tk()
 root.title("Calculator")
 
 
-e=Entry(root,width=35,borderwidth=5).grid(row=0,column=0,columnspan=3,)
+e=Entry(root,width=35,borderwidth=5)
+e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
+e.insert(0, "Enter Your Name: ")
+
+def myClick():
+    hello="Hello" + e.get()
+    label1=Label(root,text="Hello")
+    label1.pack()
+
+
+myButton=Button(root,text="Enter Your Stock Quote",command=myClick)
+
+
+root.mainloop()
