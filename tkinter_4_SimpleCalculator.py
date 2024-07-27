2a6c8bdece93c399cc08e26b6f68bc6382afba20
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-27 13:08:07 +0100

    ..

--- tkinter_4_SimpleCalculator.py
+++ tkinter_4_SimpleCalculator.py
@@ -24,19 +24,19 @@ button_0= Button(root,text="0",padx=40,pady=20,command=button_add)
 
 
 #put buttons on the screen
-button_1.grid(row=,column=)
-button_2.grid(row=,column=)
-button_3.grid(row=,column=)
+button_1.grid(row=3,column=0)
+button_2.grid(row=3,column=1)
+button_3.grid(row=3,column=2)
 
-button_4.grid(row=,column=)
-button_5.grid(row=,column=)
-button_6.grid(row=,column=)
+button_4.grid(row=2,column=0)
+button_5.grid(row=2,column=1)
+button_6.grid(row=2,column=2)
 
-button_7.grid(row=,column=)
-button_8.grid(row=,column=)
-button_9.grid(row=,column=)
+button_7.grid(row=1,column=0)
+button_8.grid(row=1,column=1)
+button_9.grid(row=1,column=2)
 
-button_0.grid(row=,column=)
+button_0.grid(row=4,column=0)
 
 
 myButton=Button(root,text="Enter Your Stock Quote",command=button_add)
