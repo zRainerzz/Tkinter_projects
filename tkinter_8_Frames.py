b1763efeb59062384809fb7f4c25e3f12c88314c
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 14:02:21 +0100

    ..

--- tkinter_8_Frames.py
+++ tkinter_8_Frames.py
@@ -13,6 +13,7 @@ icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
 root.iconphoto(True, icon_image)
 
 
-
+frame=LabelFrame(root,text="This is a Frame",padx=5,pady=10)
+frame.pack(padx=10,pady=10)
 
 root.mainloop()
