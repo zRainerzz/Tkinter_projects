3ae28cb7a7823abd8c1f51a6012ffe8ccb3172a4
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 14:18:45 +0100

    y

--- tkinter_8_Frames.py
+++ tkinter_8_Frames.py
@@ -14,8 +14,8 @@ root.iconphoto(True, icon_image)
 
 
 frame=LabelFrame(root,text="This is a Frame",padx=50,pady=100)#INTERNAL PADDING
-b1=Button(frame,text="Don't click me",command=quit)
-b2=Button(frame,text="Can't click me", state=DISABLED)
+b1=Button(frame,text="Don't click me",command=quit,bd=5)
+b2=Button(frame,text="Can't click me", state=DISABLED,bd=5)
 
 frame.pack(padx=20,pady=20)#EXTERNAL PADDING
 b1.grid(row=0,column=0)
