a984219b978729b4dfb207b4fee137b92576a81b
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 15:30:21 +0100

    ...

--- tkinter_10_MessageBox.py
+++ tkinter_10_MessageBox.py
@@ -14,14 +14,20 @@ icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
 root.iconphoto(True, icon_image)
 
 
-frame=LabelFrame(root,text="Validation",padx=50,pady=100)#INTERNAL PADDING
+frame=LabelFrame(root,text="eyes on the button not me, stalker!",padx=50,pady=100)#INTERNAL PADDING
 
 
 #showinfo, showwarning, showerror, askedquestion, askokcancel, askyesno
 def popup():
-    respone=messagebox.askyesno("UPDATE!","DONE")
+    respone=messagebox.askyesno("UPDATE!","Wanna leave a feedback?")
     Label(frame,text=respone).pack
     if respone==1:
+        mylab=Label(frame,text="you clicked Yes,Leave a Feedback!").pack()
+        e=Entry(frame,width=50,bg="black",fg="white",borderwidth=5)
+        e.pack()
+    else:
+        mylab=Label(frame,text="You clicked NO.").pack()
+
 
 
 
