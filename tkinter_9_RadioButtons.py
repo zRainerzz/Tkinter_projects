3589af74212ad0d0a67a386ea121b0f2f27f0e87
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 14:45:20 +0100

    x

--- tkinter_9_RadioButtons.py
+++ tkinter_9_RadioButtons.py
@@ -18,13 +18,23 @@ r=IntVar()#this allows tkinter to keep track to the changes overtime.
 #and in order to call r, we r.get()
 r.set("2")
 
+MODES=[
+    ("Pepperoni",""),
+    ("Cheese",""),
+    ("Mushroom",""),
+    ("Onion",""),
+]
+
+
+
 def clicked(value):
     mylab=Label(root,text=value)
+    mylab.pack()
 
 
 
-Radiobutton(root,text="option 1",variable=r,value=1,command=lambda: clicked(r.get())).pack()#variable needed in radio but
-Radiobutton(root,text="option 2",variable=r,value=2,command=lambda: clicked(r.get())).pack()#variable needed in radio but
+#Radiobutton(root,text="option 1",variable=r,value=1,command=lambda: clicked(r.get())).pack()#variable needed in radio but
+#Radiobutton(root,text="option 2",variable=r,value=2,command=lambda: clicked(r.get())).pack()#variable needed in radio but
 
 
 mylab=Label(root,text=r.get())
