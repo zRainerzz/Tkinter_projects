b3020d109713534ebd24808f43c48003e788404c
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 16:01:47 +0100

    :V

--- tkinter_11_CreateNewWindows.py
+++ tkinter_11_CreateNewWindows.py
@@ -14,13 +14,22 @@ root.iconphoto(True, icon_image)
 
 
 top=Toplevel()
+top.title("BINGO! the new window")
+icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
+top.iconphoto(True, icon_image)
+
+
 """ Load and resize image """
 img = Image.open("image2.jpg")  # Replace with your image path
-img_resized = img.resize((200, 200))
+img_resized = img.resize((400, 400))
 photo = ImageTk.PhotoImage(img_resized)
+
+
 
 """ Display image on label """
 my_label = Label(top,image=photo)
 my_label.pack()
+
+
 
 root.mainloop()
