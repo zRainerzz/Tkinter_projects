eec7b1225258bb432795c960e19b17479563bdde
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 15:09:58 +0100

    :V

--- tkinter_10_MessageBox.py
+++ tkinter_10_MessageBox.py
@@ -13,16 +13,17 @@ root.title("github.com/zRainerzz")
 icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
 root.iconphoto(True, icon_image)
 
-frame=Frame(root,Text="Validation",padx=50,pady=100)
+frame=Frame(root,Text="Validation",padx=50,pady=100)#INTERNAL PADDING
 
 
 
 def popup():
     messagebox.showinfo("DONE")
 
+frame.pack(padx=20,pady=20)#EXTERNAL PADDING
 Button(frame,text="Validation",command=popup).pack()
 
 
-frame.pack(padx=20,pady=20)
+
 
 root.mainloop()
