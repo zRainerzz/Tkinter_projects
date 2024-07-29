dd1be71f410475d932258058ef8dc3a07864307c
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 15:55:36 +0100

    00

--- tkinter_11_CreateNewWindows.py
+++ tkinter_11_CreateNewWindows.py
@@ -11,3 +11,14 @@ root.title("github.com/zRainerzz")
 """ Linux icon (replace with your .png file) """
 icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
 root.iconphoto(True, icon_image)
+
+
+top=Toplevel()
+""" Load and resize image """
+img = Image.open("image2.jpg")  # Replace with your image path
+img_resized = img.resize((200, 200))
+photo = ImageTk.PhotoImage(img_resized)
+
+lab1=Label(top, text=photo).pack
+
+root.mainloop()
