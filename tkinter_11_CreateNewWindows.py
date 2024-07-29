447934ea0bd36e9b49db02d640b86eecc0d86a03
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 15:52:18 +0100

    :V

---
+++ tkinter_11_CreateNewWindows.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+root = Tk()
+root.title("github.com/zRainerzz")
+
+""" Windows icon (replace with your .ico file) """
+# root.iconbitmap("tkinter_icon.ico")  # Uncomment for Windows
+
+""" Linux icon (replace with your .png file) """
+icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
+root.iconphoto(True, icon_image)
