2ae6a19452c91413a8f647d074ec5cc40bc0ebd4
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 18:29:52 +0100

    0

--- tkinter_12_OpenFiles-DialogBoxes.py
+++ tkinter_12_OpenFiles-DialogBoxes.py
@@ -1,8 +1,19 @@
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import filedialog
 
-root = Tk()
+def open_image():
+    filename = filedialog.askopenfilename(initialdir="/home/khedhri/Desktop/Tkinter_course",
+                                       title="Select A file.",
+                                       filetypes=(("png files", "*.png"), ("all files", "*.*")))
+    if filename:
+        img = Image.open(filename)
+        img_resized = img.resize((400, 400))
+        photo = ImageTk.PhotoImage(img_resized)
+        my_img_label.config(image=photo)
+        my_img_label.image = photo  # Keep a reference to prevent garbage collection
+
+root = tk.Tk()
 root.title("github.com/zRainerzz")
 
 """ Windows icon (replace with your .ico file) """
@@ -12,7 +23,10 @@ root.title("github.com/zRainerzz")
 icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
 root.iconphoto(True, icon_image)
 
-root.filename=filedialog.askopenfile(initialdir="/home/khedhri/Desktop/Tkinter_course")
+my_img_label = tk.Label(root)
+my_img_label.pack()
 
+open_image_button = tk.Button(root, text="Open Image", command=open_image)
+open_image_button.pack()
 
 root.mainloop()
