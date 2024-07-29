ab690f4c81928705cf8479811a7bcb46d7a3ab32
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-29 15:50:24 +0100

    ...

--- tkinter_10_MessageBox.py
+++ tkinter_10_MessageBox.py
@@ -14,14 +14,17 @@ icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
 root.iconphoto(True, icon_image)
 
 
+
+
+
 frame=LabelFrame(root,text="eyes on the button not me, stalker!",padx=50,pady=100)#INTERNAL PADDING
 
 
-#showinfo, showwarning, showerror, askedquestion, askokcancel, askyesno
+#showinfo, showwarning, showerror, askquestion, askokcancel, askyesno
 def popup():
-    respone=messagebox.askyesno("UPDATE!","Wanna leave a feedback?")
+    respone=messagebox.askquestion("UPDATE!","Wanna leave a feedback?")
     Label(frame,text=respone).pack
-    if respone==1:
+    if respone=="yes":
         mylab=Label(frame,text="you clicked Yes,Leave a Feedback!").pack()
         e=Entry(frame,width=50,bg="black",fg="white",borderwidth=5)
         e.pack()
@@ -33,9 +36,13 @@ def popup():
 
 
 frame.pack(padx=20,pady=20)#EXTERNAL PADDING
-Button(frame,text="Validation",command=popup).pack()
+Button(frame,text="Validation",command=popup,bd=3,fg="white",bg="green",width=10).pack()
+Button(frame,text="EXIT",command=quit,fg="black",bg="red",bd=3,width=10).pack()
 
 
+img = Image.open("image2.jpg")  # Replace with your image path
+img_resized = img.resize((200, 200))
+photo = ImageTk.PhotoImage(img_resized)
 
 
 root.mainloop()
