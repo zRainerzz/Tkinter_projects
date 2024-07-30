d8915696246062ffc8fbf5d596cdf66c8ff2a597
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-30 14:57:26 +0100

    .

---
+++ tkinter_15_DropdownMenus.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+root = Tk()
+root.title("github.com/zRainerzz")
+
+
+frame=LabelFrame(root,text="eyes on the button not me, stalker!",padx=50,pady=100)#INTERNAL PADDING
+
+
+""" Windows icon (replace with your .ico file) """
+# root.iconbitmap("tkinter_icon.ico")  # Uncomment for Windows
+
+""" Linux icon (replace with your .png file) """
+icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
+root.iconphoto(True, icon_image)
+
+#designating how big the original window is
+root.geometry("500x500")
+
+
+#Drop Down Boxes
+#clicked need to be defined
+clicked=StringVar()
+clicked.set("Monday")
+
+
+drop=OptionMenu(root, clicked,"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+drop.pack()
+
+
+
+
+root.mainloop()
