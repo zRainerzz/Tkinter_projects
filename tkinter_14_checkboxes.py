a6c78c2de4f17ad3f2ea33880b923707834782d6
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-30 13:39:30 +0100

    :V

---
+++ tkinter_14_checkboxes.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+root = Tk()
+root.title("github.com/zRainerzz")
+
+
+frame=LabelFrame(root,text="eyes on the button not me, stalker!",padx=50,pady=100)#INTERNAL PADDING
+
+
+""" Windows icon (replace with your .ico file) """
+# root.iconbitmap("tkinter_icon.ico")  # Uncomment for Windows
+
+""" Linux icon (replace with your .png file) """
+icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
+root.iconphoto(True, icon_image)
+
+#designating how big the original window is
+root.geometry("500x500")
