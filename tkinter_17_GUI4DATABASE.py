4a2ba05cbbca21ceb3888f62a09cb745e239a793
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-30 15:31:03 +0100

    .

---
+++ tkinter_17_GUI4DATABASE.py
@@ -0,0 +1,51 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import sqlite3
+
+
+root = Tk()
+root.title("github.com/zRainerzz")
+
+
+frame=LabelFrame(root,text="eyes on the button not me, stalker!",padx=50,pady=100)#INTERNAL PADDING
+
+
+""" Windows icon (replace with your .ico file) """
+# root.iconbitmap("tkinter_icon.ico")  # Uncomment for Windows
+
+""" Linux icon (replace with your .png file) """
+icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
+root.iconphoto(True, icon_image)
+
+#designating how big the original window is
+root.geometry("500x500")
+
+
+#Databases
+
+
+#Create a database or connect to one.
+conn=sqlite3.connect('tkinter_17_GUI4DATABASE.db')
+
+#Create cursor
+c=conn.cursor()
+
+#Create table
+c.execute("""CREATE TABLE adresses (
+          first_name text,
+          last_name text,
+          adress text,
+          city text,
+          state text,
+          zipcode integer
+          
+)""")
+
+#Commit changes
+conn.commit()
+
+#Close connection
+conn.close
+
+
+root.mainloop()
