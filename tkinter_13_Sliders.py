45c73d8673d68c9f2fa9181a80f8a5fc0f0bb18c
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-30 12:35:46 +0100

    .

--- tkinter_13_Sliders.py
+++ tkinter_13_Sliders.py
@@ -20,8 +20,15 @@ root.iconphoto(True, icon_image)
 root.geometry("500x500")
 
 
+def slide1():
+    mylab=Label(root,text=vertical.get()).pack()
+    root.geometry(str(vertical.get())+"x400")
+def slide2():
+    mylab2=Label(root,text=horizontal.get()).pack()
+    root.geometry(str(horizontal.get())+"x400")
+
 vertical=Scale(root,from_=0,to=800)
-horizontal=Scale(root,from_=0,to=800,orient=HORIZONTAL)
+horizontal=Scale(root,from_=0,to=800,orient=HORIZONTAL,command=slide2)
 
 vertical.pack()
 horizontal.pack()
@@ -30,15 +37,10 @@ mylab=Label(root,text=vertical.get()).pack()
 mylab2=Label(root,text=horizontal.get()).pack()
 
 
-def slide1():
-    mylab=Label(root,text=vertical.get()).pack()
-    root.geometry(str(vertical.get())+"x400")
-def slide2():
-    mylab2=Label(root,text=horizontal.get()).pack()
-    root.geometry(str(horizontal.get())+"x400")
 
-btn1=Button(root,text="Slide vertically",command=slide1).pack()
-btn2=Button(root,text="Slide horizentally",command=slide2).pack()
+
+#btn1=Button(root,text="Slide vertically",command=slide1).pack()
+#btn2=Button(root,text="Slide horizentally",command=slide2).pack()
 
 
 
