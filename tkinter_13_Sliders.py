ea0c2201e461e0f2bfeb38479fb19a65f21510a3
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-30 12:30:54 +0100

    ..

--- tkinter_13_Sliders.py
+++ tkinter_13_Sliders.py
@@ -32,9 +32,10 @@ mylab2=Label(root,text=horizontal.get()).pack()
 
 def slide1():
     mylab=Label(root,text=vertical.get()).pack()
-
+    root.geometry(str(vertical.get())+"x400")
 def slide2():
     mylab2=Label(root,text=horizontal.get()).pack()
+    root.geometry(str(horizontal.get())+"x400")
 
 btn1=Button(root,text="Slide vertically",command=slide1).pack()
 btn2=Button(root,text="Slide horizentally",command=slide2).pack()
