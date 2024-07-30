7b493e80cdcdd2b8c3adb160b38d89d6cdb5b028
Ghaith <ghaithkhedhri3@gmail.com>
2024-07-30 15:16:15 +0100

    .

---
+++ tkinter_16_DATABASES.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import sqlite3
+
+
+root = Tk()
+root.title("github.com/zRainerzz")
+
+
+frame=LabelFrame(root,text="eyes on the button not me, stalker!",padx=50,pady=100)#INTERNAL PADDING
+
+
+""" Windows icon (replace with your .ico file) """
+# root.iconbitmap("tkinter_icon.ico")  # Uncomment for Windows
+
+""" Linux icon (replace with your .png file) """
+icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
+root.iconphoto(True, icon_image)
+
+#designating how big the original window is
+root.geometry("500x500")
+
+
+#Databases
+
+
+#Create a database or connect to one.
+conn=sqlite3.connect('address_book.db')
+
+
+
+
+
+root.mainloop()
