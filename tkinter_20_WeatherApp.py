003844a3f9dbb4a4b6a20e43dc72fcdd67f3dac4
Ghaith <ghaithkhedhri3@gmail.com>
2024-08-01 22:52:51 +0100

    QS

--- tkinter_20_WeatherApp.py
+++ tkinter_20_WeatherApp.py
@@ -12,9 +12,14 @@ icon_image = ImageTk.PhotoImage(Image.open("tkinter_icon.png"))
 root.iconphoto(True, icon_image)
 
 #designating how big the original window is
-root.geometry("500x500")
+root.geometry("400x50")
 
 #Frame
-frame=LabelFrame(root,text="This is a Frame",padx=50,pady=100)#INTERNAL PADDING
+frame=LabelFrame(root,text="This is a Frame",padx=5,pady=5)#INTERNAL PADDING
+
+#https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=9BE25E99-5FC4-4F57-B4AD-72DC0518FE67
+
+
+frame.pack(padx=20,pady=20)#EXTERNAL PADDING
 
 root.mainloop()
