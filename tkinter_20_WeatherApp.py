14688ad55872a697fb6a42d713d82118f8af53ea
Ghaith <ghaithkhedhri3@gmail.com>
2024-08-01 22:56:44 +0100

    s

--- tkinter_20_WeatherApp.py
+++ tkinter_20_WeatherApp.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk
+import requests
+import json
 
 root = Tk()
 root.title("github.com/zRainerzz")
@@ -17,7 +19,9 @@ root.geometry("400x50")
 #Frame
 frame=LabelFrame(root,text="This is a Frame",padx=5,pady=5)#INTERNAL PADDING
 
-#https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=9BE25E99-5FC4-4F57-B4AD-72DC0518FE67
+#Creating a request, it has my API Key , i'll delete it once i finish recording, go to airnowapi and make your own account.
+api_req=requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=9BE25E99-5FC4-4F57-B4AD-72DC0518FE67")
+
 
 
 frame.pack(padx=20,pady=20)#EXTERNAL PADDING
