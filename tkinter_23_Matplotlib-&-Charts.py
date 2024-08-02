35f43633b818c6a8922eae70be3df60c80652452
Ghaith <ghaithkhedhri3@gmail.com>
2024-08-02 01:38:25 +0100

    sd

--- tkinter_23_Matplotlib-&-Charts.py
+++ tkinter_23_Matplotlib-&-Charts.py
@@ -16,13 +16,20 @@ root.iconphoto(True, icon_image)
 #designating how big the original window is
 root.geometry("400x200")
 
-def graph():
+def graph1():
     house_prices=np.random.normal(200000,25000,5000)
     plt.hist(house_prices, 250)
     plt.show()
 
+def graph2():
+    house_prices1=np.random.normal(200000,25000,5000)
+    plt.pie(house_prices1)
+    plt.show()
 
-my_but=Button(root,text='Graphic',command=graph)
+my_but=Button(root,text='Graphic',command=graph1)
+my_but2=Button(root,text='PIE GRAPHIC',command=graph2)
+
 my_but.pack()
+my_but2.pack()
 
 root.mainloop()
