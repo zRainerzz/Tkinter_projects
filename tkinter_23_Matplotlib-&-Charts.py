1b05eedd4b58f2d6377d69364a101b22decd5907
Ghaith <ghaithkhedhri3@gmail.com>
2024-08-02 01:35:09 +0100

    sd

--- tkinter_23_Matplotlib-&-Charts.py
+++ tkinter_23_Matplotlib-&-Charts.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import requests
-import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 root = Tk()
 root.title("github.com/zRainerzz")
@@ -16,6 +16,13 @@ root.iconphoto(True, icon_image)
 #designating how big the original window is
 root.geometry("400x200")
 
+def graph():
+    house_prices=np.random.normal(200000,25000,5000)
+    plt.hist(house_prices, 250)
+    plt.show()
 
+
+my_but=Button(root,text='Graphic',command=graph)
+my_but.pack()
 
 root.mainloop()
